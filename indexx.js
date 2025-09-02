696254fb3f32c19970340d90b24598408ecaba2e
Raghav6571 <147227411+Raghav6571@users.noreply.github.com>
2025-09-02 19:23:56 +0530

    Update indexx.js

--- indexx.js
+++ indexx.js
@@ -1,37 +1,35 @@
 var timeleft = 0
 var flag = true 
-var time = 10
 document.querySelector(".button2").addEventListener("click" , function (event) {
             
-        if(timeleft >0) {
-            timeleft--;
-        if(timeleft >= 0) {
+        if(timeleft > 0) {
         // console.log(timeleft)
-        document.querySelector("h2").innerHTML = timeleft;
-
-            }
+        timeleft--;
+        document.querySelector("h2").innerHTML = timeleft;  
         }
+        
         
 })
 
 document.querySelector(".button1").addEventListener("click" , function() {
            
-        if(timeleft < time) {
-            timeleft++;
-        if(timeleft <= time) {
+        if(flag) {
         // console.log(timeleft);
+        timeleft++;
         document.querySelector("h2").innerHTML = timeleft;
-        }
         }
 })
 
 document.querySelector(".button3").addEventListener("click", function(){
-    
+        
         for(var i=timeleft; i > 0; i--){
            
             setTimeout(function () {
-            timeleft--;
+            if(timeleft > 0)
+        {
+                timeleft--;       }
             document.querySelector("h2").innerHTML = timeleft;
             },i* 200);
-        }    
+        }
+    
 })
